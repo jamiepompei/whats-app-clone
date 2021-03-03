b963b16d9e54dce854b6536fdfa3222f8acae4a2
Nicole Pompei <nicole.lynn.pompei@gmail.com>
2021-03-03 18:27:38 -0500

    added server.js and procfile

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extdned': 'true'}));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'POST, GET');
+    res.header("Access-Control-Allows-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 5000);
+app.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+})
